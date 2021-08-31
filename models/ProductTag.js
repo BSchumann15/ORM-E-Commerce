911cbf7cdf84f21ad6167780bc25f21aca3067fb
Blake Schumann <blakeschumann15@gmail.com>
2021-08-31 19:37:12 +0930

    defining columns and adding comments

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,28 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+  },
+  // defining the product_id column
+  product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+          model: "product",
+          key: "id"
+      }
+  },
+  // defining the tag_id column
+  tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+          model: "tag",
+          key: "id"
+      }
+  }
   },
   {
     sequelize,
